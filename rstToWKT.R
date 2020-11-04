29e9bf49831647e935f61a192539f0bd08df3264
Huriel Reichel <53496565+hurielreichel@users.noreply.github.com>
2020-11-04 14:44:32 +0100

    Updating Copyrights

--- rstToWKT.R
+++ rstToWKT.R
@@ -1,7 +1,7 @@
 # rstToWKT
 
   # Huriel Reichel
-  # Copyright (c) 2020 STDL, Swiss Territorial Data Lab
+  # Copyright (c) 2020 Republic and Canton of Geneva
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
